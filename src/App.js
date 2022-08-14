49634a51d493083a12db56b2ea3f1b211ac33042
JonathanMRuiz <jonimrz@yahoo.com>
2022-08-14 18:32:20 -0300

    fetch infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home";
+import { QueryClient, QueryClientProvider } from "react-query";
+import GetData from "./api/GetData";
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div>
       <Home />
+      <QueryClientProvider client={queryClient}>
+        <GetData />
+      </QueryClientProvider>
     </div>
   );
 }
